6b1b5d3191a4578c44eea3c662c32cee5a6a43b3
Pahan <pahanchathuranga123@gmail.com>
2021-05-23 14:43:50 +0530

    :heavy_check_mark:Task-Tracker completed.

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 import { Task } from '../../Task';
 
 @Component({
@@ -12,9 +14,12 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask:boolean ;
+  subscription:Subscription ;
 
-
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+   }
 
   ngOnInit(): void {
   }
